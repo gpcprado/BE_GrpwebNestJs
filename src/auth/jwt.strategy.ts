b8b334e73350da32c7b6f501d2d9db24ce6696eb
Samuel Prado <gpcprado@gbox.ncf.edu.ph>
2025-12-07 04:20:42 +0800

    second commit

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET || 'access_secret',
     });
   }
 
