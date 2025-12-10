c0440d70fba6a19409f8fb08bdf89cd0787fe6bf
gpcprado <gpcprado@gbox.ncf.edu.ph>
2025-12-11 01:35:41 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,6 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3001;
-  app.enableCors({
-    origin: [
-      "http://localhost:3000",
-      "http://192.168.10.109:3000",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-      
   await app.listen(+port);
   console.log(`🚀 Server listening on http://localhost:${port}`);
 }
