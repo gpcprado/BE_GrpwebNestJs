43a1eac6e5b9bfad152af0dad7ce649c049bdc1a
Samuel Prado <gpcprado@gbox.ncf.edu.ph>
2025-12-11 00:51:55 +0800

    third commit

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,11 @@ import 'reflect-metadata';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   app.enableCors({
     origin: [
       "http://localhost:3000",
       "http://192.168.10.109:3000",
-      "http://localhost:3001",
-      "http://192.168.10.109:3001",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
